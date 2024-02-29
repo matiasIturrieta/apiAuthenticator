280653b16a2ade4d171e918d84d6d314582c77f8
unknown <vu_miturrieta@externos.entel.cl>
2024-02-29 14:20:37 -0300

    creacion de INC EntelQA ServiceNow

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const axios = require('axios'); // Importa Axios
+
+
 
 const app = express();
 
-
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 const users = [];
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 448;
 
 app.post('/registro', async (req, res) => {
     const { username, password } = req.body;
@@ -43,6 +49,38 @@ app.get('/protected', (req, res) => {
     res.send('Protected route');
 });
 
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+const httpsAgent = new require('https').Agent({
+    rejectUnauthorized: false // Ignora la validación del certificado SSL
+});
+
+
+app.post('/CrearINC', async (req, res) => {
+    // Aquí asumimos que el body ya viene formateado correctamente desde el cliente
+    const incidentData = req.body;
+      
+    const config = {
+        method: 'post',
+        url: 'https://entelqa.service-now.com/api/now/table/incident?sysparm_input_display_value=true',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            // Aquí se configura la autenticación básica
+            'Authorization': 'Basic ' + Buffer.from(`${process.env.USER}:${process.env.PASSWORD}`).toString('base64')
+        },
+        data : incidentData,
+        httpsAgent: httpsAgent
+    };
+
+    try {
+        const response = await axios(config);
+        res.json({ success: true, data: response.data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Error al enviar la incidencia' });
+    }
 });
