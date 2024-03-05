b599c49a55799ea8e270355217cfa6fa2df46bf9
unknown <vu_miturrieta@externos.entel.cl>
2024-03-05 14:39:19 -0300

    Incidente Service Now v1

--- app.js
+++ app.js
@@ -1,86 +1,163 @@
-require('dotenv').config();
-
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const axios = require('axios'); // Importa Axios
+const axios = require('axios');
+const dotenv = require('dotenv');
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const fs = require("fs");
+const https = require('https');
 
-
+dotenv.config();
 
 const app = express();
-
-app.use(bodyParser.json());
-
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('public'));
 
-const users = [];
 const PORT = process.env.PORT || 448;
+const subscriptionKey = process.env.SPEECH_KEY;
+const serviceRegion = process.env.SPEECH_REGION;
+const filename = process.env.FILE_NAME_LOCAL;
 
-app.post('/registro', async (req, res) => {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    
-    // Simulación de guardado en base de datos
-    const user = { username, password: hashedPassword };
-    users.push(user);
+console.log("Transcribing from: " + filename);
 
-    res.status(201).send({ message: 'User created' ,PORT });
-});
+let testBuffer = fs.readFileSync(filename);
+console.log(testBuffer);
 
-// Ruta de login
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username);
-    
-    if (user && await bcrypt.compare(password, user.password)) {
-        // Generar un token
-        const token = jwt.sign({ username: user.username }, 'secret', { expiresIn: '1h' });
-        res.status(200).send({ message: 'Authenticated', token , PORT });
-    } else {
-        res.status(400).send({ message: 'User or password is incorrect' });
-    }
-});
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-// Ruta protegida
-app.get('/protected', (req, res) => {
-    // Lógica para verificar el token
-    // ...
-    res.send('Protected route');
-});
+var speechConfig = sdk.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
+speechConfig.speechRecognitionLanguage = "es-MX";
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const usuariosArray =  process.env.ArrUsuarios.split(',');
+const passwordsArray = process.env.ArrPassword.split(',');
 
-const httpsAgent = new require('https').Agent({
-    rejectUnauthorized: false // Ignora la validación del certificado SSL
-});
+// Crea un objeto que mapee usuarios a contraseñas
+const users = usuariosArray.reduce((obj, usuario, index) => {
+  obj[usuario] = passwordsArray[index];
+  return obj;
+}, {});
 
+console.log(users);
 
-app.post('/CrearINC', async (req, res) => {
-    // Aquí asumimos que el body ya viene formateado correctamente desde el cliente
-    const incidentData = req.body;
-      
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
+async function handleCrearINC(incidentData, res) {
     const config = {
         method: 'post',
         url: 'https://entelqa.service-now.com/api/now/table/incident?sysparm_input_display_value=true',
         headers: { 
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            // Aquí se configura la autenticación básica
             'Authorization': 'Basic ' + Buffer.from(`${process.env.USERNAMEAPI}:${process.env.PASSWORDAPI}`).toString('base64')
         },
-        data : incidentData,
+        data: {
+            "sys_domain": "6ad48d221b4564101df3bb7f034bcb57",
+            "company": "Comercial Kaufmann S A",
+            "caller_id": "coe bot 004",
+            "state": "Nuevo",
+            "u_std_responsible": "Entel",
+            "business_service": "Serv Ti - Incidencias Generales",
+            "service_offering": "Reportar Incidente",
+            "u_gbl_service_string": "Reportar Incidente",
+            "category": "Incidentes Plataformas, De Eventos Y Monitoreo",
+            "subcategory": "Falla Servicio",
+            "u_std_fail_condition": "sin_perdida",
+            "impact": "2-Significativo/Amplio",
+            "urgency": "4-Baja",
+            "assignment_group": "Sdc_Sop_N1_Inc",
+            "short_description": "Incidente generado por IA",
+            "description": `${incidentData.text}`,
+            "u_gbl_operational_unit": "SSGG"
+        },
         httpsAgent: httpsAgent
     };
 
     try {
         const response = await axios(config);
-        res.json({ success: true, data: response.data });
+        console.log("Response from ServiceNow:", response.data);
+        res.status(200).json({ success: true, message: 'Incidencia creada correctamente', data: response.data });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, message: 'Error al enviar la incidencia' });
     }
+}
+
+async function fromBufferOrBase64(audioInput, res) {
+    const buffer = (typeof audioInput === 'string') ? Buffer.from(audioInput, 'base64') : audioInput;
+    let audioConfig = sdk.AudioConfig.fromWavFileInput(buffer);
+    let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+
+    speechRecognizer.recognizeOnceAsync(result => {
+        if (result.reason === sdk.ResultReason.RecognizedSpeech) {
+            console.log(`RECOGNIZED: Text=${result.text}`);
+            handleCrearINC({ text: result.text }, res);
+        } else {
+            console.error("Error during speech recognition:", result);
+            res.status(500).json({ success: false, message: 'Error durante la transcripción del audio' });
+        }
+        speechRecognizer.close();
+    });
+}
+//Transforma buffer a audio
+app.post('/login', async (req, res) => {
+    const { username, password, audioBuffer } = req.body;
+
+    if (users[username] && password === users[username]) {
+        
+        try {
+            // Verifica si audioBuffer está presente y no está vacío
+            if (audioBuffer) {
+                // Convierte la cadena que representa el array a un array real
+                const audioArray = JSON.parse(audioBuffer);
+                
+                // Convierte el array a Buffer
+                const buffer = Buffer.from(audioArray);
+
+                await fromBufferOrBase64(buffer, res);
+            } else {
+                res.status(200).send({ message: 'Authenticated', token });
+            }
+        } catch (error) {
+            console.error("Error during speech recognition:", error);
+            res.status(500).send({ message: 'Internal Server Error', error: error.message });
+        }
+    } else {
+        res.status(400).send({ message: 'User or password is incorrect' });
+    }
+});
+
+/**
+ * Transforma base64 a audio
+ * app.post('/login', async (req, res) => {
+    const { username, password, audioBuffer } = req.body;
+    const user = users.find(u => u.username === username);
+
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ username: user.username }, 'secret', { expiresIn: '1h' });
+
+        try {
+            if (audioBuffer) {
+                await fromBufferOrBase64(audioBuffer, res);
+            } else {
+                res.status(200).send({ message: 'Authenticated', token });
+            }
+        } catch (error) {
+            console.error("Error during speech recognition:", error);
+            res.status(500).send({ message: 'Internal Server Error' });
+        }
+    } else {
+        res.status(400).send({ message: 'User or password is incorrect' });
+    }
+});
+
+ * 
+ */
+
+// Tu código restante aquí...
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
